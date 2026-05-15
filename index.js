5453ccdc6463b6f0c117e6bc282d6a8e66874718
Emerance63 <iribagizaemerance63@gmail.com>
2026-05-15 15:21:12 +0200

    Integrate Local Storage for saving and retrieving events

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // --------localStorage-------
 
-let student = JSON.parse(localStorage.getItem("StudentDashboard"))||[
-    {
-        id: 1,
-        title: "AI Bootcamp",
-        category: "Technology",
-        seats: 30,
-        registered: 12
-      },
-      {
-        id: 2,
-        title: "Football Tournament",
-        category: "Sports",
-        seats: 50,
-        registered: 20
-      },
-      {
-        id: 3,
-        title: "Music Festival",
-        category: "Music",
-        seats: 40,
-        registered: 15
-      }
+let StudentDashboard = JSON.parse(localStorage.getItem("StudentDashboard")) || [
+  {
+    id: 1,
+    title: "AI Bootcamp",
+    category: "Technology",
+    seats: 30,
+    registered: 12,
+  },
+  {
+    id: 2,
+    title: "Football Tournament",
+    category: "Sports",
+    seats: 50,
+    registered: 20,
+  },
+  {
+    id: 3,
+    title: "Music Festival",
+    category: "Music",
+    seats: 40,
+    registered: 15,
+  },
 ];
-// localStorage.setItem("studentDashboard",JSON.stringify());
 
+// ------Save to Localstorage-----
 
+function saveToLocalStorage() {
+  localStorage.setItem("StudentDashboard", JSON.stringify(StudentDashboard));
+}
+saveToLocalStorage();
