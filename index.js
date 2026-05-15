108ae657d16dce8d176e34d628d614dacb058fea
Emerance63 <iribagizaemerance63@gmail.com>
2026-05-15 16:44:06 +0200

    add add button for showing form

--- index.js
+++ index.js
@@ -31,35 +31,46 @@ function saveToLocalStorage() {
 }
 saveToLocalStorage();
 
-const addeventBtn =document.getElementById("addeventBtn");
-const totalEvent =document.getElementById("totalEvent");
-const totalRegistered =document.getElementById("totalRegistered");
-const availableseat =document.getElementById("availableSeat");
+const addeventBtn = document.getElementById("addeventBtn");
+const totalEvent = document.getElementById("totalEvent");
+const totalRegistered = document.getElementById("totalRegistered");
+const availableseat = document.getElementById("availableSeat");
 const addEventForm = document.getElementById("addEventForm");
-const eventTitle =document.getElementById("eventTitle");
-const eventCategory =document.getElementById("eventCategory");
-const eventSeats =document.getElementById("eventSeats");
-const formErrorMsg =document.getElementById("formErrorMsg");
-const eventCountBadge =document.getElementById("eventCountBadge");
-const noEventsMessage =document.getElementById("noEventsMessage");
+const eventTitle = document.getElementById("eventTitle");
+const eventCategory = document.getElementById("eventCategory");
+const eventSeats = document.getElementById("eventSeats");
+const formErrorMsg = document.getElementById("formErrorMsg");
+const eventCountBadge = document.getElementById("eventCountBadge");
+const noEventsMessage = document.getElementById("noEventsMessage");
 
 // -------statistics--------
 
- function updateStatistics() {
-
+function updateStatistics() {
   totalEvent.textContent = StudentDashboard.length;
 
   const registered = StudentDashboard.reduce(
     (total, event) => total + event.registered,
-    0
+    0,
   );
 
   totalRegistered.textContent = registered;
 
   const remaining = StudentDashboard.reduce(
     (total, event) => total + (event.seats - event.registered),
-    0
+    0,
   );
 
   availableseat.textContent = remaining;
 }
+
+// ---------addbtn------
+
+addeventBtn.addEventListener("click", function () {
+  if(addEventForm.style.display === "none"){
+    addEventForm.style.display = "block";
+  } else{
+    addEventForm.style.display = "none";
+  }
+  
+});
+
